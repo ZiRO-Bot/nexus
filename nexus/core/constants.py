549b659eed36234b37b3d9c7efc43535ae9227b7
ziro <palembani@gmail.com>
2023-07-24 15:11:43 +0700

    fix: DEBUG always return True

--- nexus/core/constants.py
+++ nexus/core/constants.py
@@ -1,4 +1,5 @@
 import os
+from nexus.utils.humanize import str_bool
 
 
 REQUEST_TIMEOUT = 2500
@@ -6,6 +7,6 @@ REQUEST_RETRIES = 3
 PREFIX_V1 = "/v1"
 PREFIX_V2 = "/v2"
 NEXUS_EPOCH = 1689740330
-DEBUG = bool(os.getenv("DASHBOARD_IS_DEBUG", 0))
+DEBUG = str_bool(os.getenv("DASHBOARD_IS_DEBUG", "0"))
 SESSION_EXPIRY = 14 * 24 * 60 * 60  # 14 days
 SESSION_EXPIRY_DEBUG = 24 * 60 * 60
